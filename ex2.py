3750c4fa727757e00e66008745fcad8d354934a3
Michael Carlson <mwctahoe@gmail.com>
2012-09-28 21:11:14 -0500

    exercise 1 and 2
    running versions of exercise 1 and 2

---
+++ ex2.py
@@ -0,0 +1,8 @@
+#you dawg commenting all up in here
+
+print "I could have code like this." #and can't touch this stuff
+
+#stop comment time
+#print "this aint gonna run"
+
+print "but this will. YAY basics!"
