db9b9f4c165ead2966fb96bcdbdb59ea009a16c9
Michael Carlson <mwctahoe@gmail.com>
2012-10-11 20:25:46 -0500

    ex5
    printf style

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = 'mwctahoe'
+my_age = 22
+my_height = 74
+my_weight = 180
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print "let's talk about %s." % my_name
+print "he's %d inches tall." % my_height
+print "he;s %d pounds heavy." % my_weight
+print "actually thats not too heavy."
+print "he's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "his teeth are usually %s depending on the coffee." % my_teeth
+
+print "if i add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
