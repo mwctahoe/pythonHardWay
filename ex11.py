ef043a4a395c011914d92e8ec905e675eea34407
Michael Carlson <mwctahoe@gmail.com>
2012-10-15 19:42:16 -0500

    ex11

---
+++ ex11.py
@@ -0,0 +1,14 @@
+print "Got some questions for you now."
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+print "write something here?"
+quote = raw_input()
+
+print quote
