88673ac49fbabbf8020bf6fcaae3c42acb3605f2
Michael Carlson <mwctahoe@gmail.com>
2013-01-06 21:20:10 -0600

    ex 40 and start of 41

--- mystuff.py
+++ mystuff.py
@@ -1,4 +1,13 @@
-def apple():
-	print "I AM APPLES!"
-	
-tangerine = "Living reflection of a dream"
+class MyStuff(object):
+
+	def __init__(self):
+		self.tangerine = "And now a thousand years between"
+		
+	def apple(self):
+		print "HAVE AT THEE VILE VILLAN"
+		
+		
+thing = MyStuff()
+thing.apple()
+print thing.tangerine
+
